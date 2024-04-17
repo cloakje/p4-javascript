4b12943ff05fe3c0a384db6fdfbfd888c16758e6
bart <9019875@student.zadkine.nl>
2024-04-17 12:54:08 +0200

    pokemon

---
+++ pokemon (1)/pokemon/app.js
@@ -0,0 +1,12 @@
+let output = document.querySelector(".output");
+let out = "";
+fetch("./pokemon.json").
+then((response) => response.json()).then((pokemon) => {
+  for (let pokemons of pokemon) {
+    out += `
+<h1> ${pokemons.name}</h1>
+<img src=${pokemons.img} alt="" />
+`;
+  }
+  output.innerHTML = out;
+});
